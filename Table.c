28c904054e689718bc26642b317f302513ee8d3c
AdeebaRahman238 <156448028+AdeebaRahman238@users.noreply.github.com>
2024-03-26 21:37:22 +0530

    Table.c

---
+++ Table.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {  
+int num; // Take the number as an input from the user 
+ printf("Enter the value of number whose multiplication table is to be printed\n");
+  scanf("%d", &num);
+  printf("The multiplication table of %d is\n", num);
+ 
+  for (int i = 0; i < 10; i++)
+    {
+      printf("%d X %d = %d\n",num, i+1, (i+1)*num);
+    }
+  return 0;
+}
